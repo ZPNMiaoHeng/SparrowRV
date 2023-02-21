e0b2bae58dc8b15ba3ed4e7a1918e56baac049a8
xiaowuzxc <wuyan-100@163.com>
2023-02-21 21:12:37 +0800

    可读性改进

--- tb/tools/isa_test.py
+++ tb/tools/isa_test.py
@@ -5,8 +5,8 @@ from tkinter import filedialog
 
 def 找到所有bin文件(path):
     找到的文件列表 = []
-    list_dir = os.walk(path)
-    for 主目录, 子目录, 所有文件 in list_dir:
+    收集文件 = os.walk(path)
+    for 主目录, 子目录, 所有文件 in 收集文件:
         for 文件的名称 in 所有文件:
             找到的文件 = os.path.join(主目录, 文件的名称)
             if 找到的文件.endswith('.bin'):
@@ -124,17 +124,17 @@ def iverilog指令集测试():
     开始时间 = time.time()
     bin文件列表 = 找到所有bin文件(r'tools/isa/generated')
     错误标志 = False
-    for file in bin文件列表:
-        输出字符串 = ISA测试(file)
+    for 待测bin文件 in bin文件列表:
+        输出字符串 = ISA测试(待测bin文件)
         if (输出字符串.find('TEST_PASS') != -1):
-            print('项目 ' + file[29:] + '   通过')
+            print('项目 ' + 待测bin文件[29:] + '   通过')
         else:
             print('  ------------------------------------  ')
-            print('----     项目 ' + file[29:] + '    出错     ----')
+            print('----     项目 ' + 待测bin文件[29:] + '    出错     ----')
             print('  ------------------------------------  ')
             print(输出字符串)
             print('  ------------------------------------  ')
-            print('----     打开 ' + file[29:] + '    波形     ----')
+            print('----     打开 ' + 待测bin文件[29:] + '    波形     ----')
             print('  ------------------------------------  ')
             显示波形 = os.popen(r'gtkwave tb.vcd')
             显示波形.close()
@@ -151,17 +151,17 @@ def modelsim指令集测试():
     vsim编译进程 = os.popen(r'vsim -c -do vsim_regress_cpl.tcl')
     vsim编译输出 = vsim编译进程.read()
     vsim编译进程.close()
-    for file in bin文件列表:
-        输出字符串 = modelsim_ISA测试(file)
+    for 待测bin文件 in bin文件列表:
+        输出字符串 = modelsim_ISA测试(待测bin文件)
         if (输出字符串.find('TEST_PASS') != -1):
-            print('项目 ' + file[29:] + '   通过')
+            print('项目 ' + 待测bin文件[29:] + '   通过')
         else:
             print('  ------------------------------------  ')
-            print('----     项目 ' + file[29:] + '    出错     ----')
+            print('----     项目 ' + 待测bin文件[29:] + '    出错     ----')
             print('  ------------------------------------  ')
             print(输出字符串)
             print('  ------------------------------------  ')
-            print('----     打开 ' + file[29:] + '    波形     ----')
+            print('----     打开 ' + 待测bin文件[29:] + '    波形     ----')
             print('  ------------------------------------  ')
             显示波形 = os.popen(r'gtkwave tb.vcd')
             显示波形.close()
