fc009923dbf061246a0c2f2e5fe531ff24ffb42f
xiaowuzxc <wuyan-100@163.com>
2023-03-07 14:06:34 +0800

    增强printf显示效果

--- tb/tb_core.sv
+++ tb/tb_core.sv
@@ -5,28 +5,36 @@ module tb_core();
 `define CorePath inst_sparrow_soc.inst_core
 
 
-//测试用信号
-logic clk;
-logic randem;
-logic rst_n;
+//测试DUT信号
+logic clk;//时钟信号
+logic randem;//产生异步信号
+logic rst_n;//复位
 logic core_ex_trap_valid, core_ex_trap_ready;//外部中断线
 logic JTAG_TCK,JTAG_TMS,JTAG_TDI,JTAG_TDO;//jtag
 wire spi0_miso;
 wire [31:0]fpioa;
 
+//仿真显示信号
 logic [63:0] sim_cycle_cnt = '0;//仿真周期计数器
+localparam sim_printf_line_length = 64;//显示printf的最大单行字符数
+int sim_printf_p = sim_printf_line_length;//仿真csr printf的显示终端指针
+logic [7:0] sim_printf_ascii = '0;//仿真csr printf的字符
+logic [sim_printf_line_length*8-1:0] sim_printf_line = '0;//仿真csr printf的显示终端，和处理器运行状态同步
 
 //assign fpioa[3:2] = 0;
-wire uart0_rx= (sim_cycle_cnt>21045 && sim_cycle_cnt<50000)?randem:1'b1;//1'b1;//fpioa[0]
+
+wire uart0_rx= (sim_cycle_cnt>21045 && sim_cycle_cnt<50000)?randem:1'b1;//测试uart rx
 assign fpioa[0]=uart0_rx;
+
 wire uart0_tx=fpioa[1];//fpioa[1]
+
+//spi
 assign fpioa[4]=spi0_miso;
 wire spi0_mosi=fpioa[5];
 wire spi0_clk=fpioa[6];
 wire spi0_cs=fpioa[7];
 
-
-
+//测试信号
 assign fpioa[31] = 1'b1;
 
 integer r;//计数工具人
@@ -38,10 +46,10 @@ wire mends = `CorePath.inst_csr.mends;//仿真结束标志
 
 // 读入程序
 initial begin
-    for(r=0; r<`IRamSize; r=r+1) begin
+    for(r=0; r<`IRamSize; r=r+1) begin//先填充0
         `CorePath.inst_iram.inst_appram.BRAM[r] = 32'h0;
     end
-    $readmemh ("inst.txt", `CorePath.inst_iram.inst_appram.BRAM);
+    $readmemh ("inst.txt", `CorePath.inst_iram.inst_appram.BRAM);//把程序(inst.txt)写进去
 /*
     `CorePath.inst_iram.inst_appram.BRAM[0] = 32'h000027b7;
     `CorePath.inst_iram.inst_appram.BRAM[1] = 32'h00078067;//无启动文件，跳转至0x2000
@@ -56,13 +64,14 @@ initial begin
     clk = '0;
     forever #(5) clk = ~clk;
 end
+
 // 生成异步信号
 initial begin
     randem = '0;
     forever #(7) randem = ~randem;
 end
 
-
+//仿真周期计数
 always @(posedge clk) 
     sim_cycle_cnt <= sim_cycle_cnt+1;
 
@@ -74,7 +83,7 @@ initial begin
     #10;
 `ifdef ISA_TEST  //通过宏定义，控制是否是指令集测试程序
     wait(x26 == 32'b1);   // x26 == 1，结束仿真
-    @(posedge clk);
+    @(posedge clk);//等3个周期
     @(posedge clk);
     @(posedge clk);
     if (x27 == 32'b1) begin
@@ -107,11 +116,11 @@ initial begin
 `endif
 end
 
-initial begin
+initial begin//超时强制结束
 `ifndef MODELSIM
     #600000;//iverilog
 `else 
-    #3000000;//modeslsim
+    #3000000;//modeslsim速度快，长一个数量级
 `endif
 `ifdef ISA_TEST
     $display("*Sim tool:ISA_TEST Timeout, Err");//ISA测试超时
@@ -133,11 +142,11 @@ initial begin
 end
 
 task sysrst;//复位任务
-    core_ex_trap_valid=0;
-    JTAG_TCK=0;
+    core_ex_trap_valid=0;//关闭外部中断线
+    JTAG_TCK=0;//JTAG不工作
     JTAG_TMS=0;
     JTAG_TDI=0;
-    rst_n <= '0;
+    rst_n <= '0;//复位
     #15
     rst_n <= '1;
     #10;
@@ -145,10 +154,10 @@ endtask : sysrst
 
 task ex_trap;//外部中断
     #15000;
-    core_ex_trap_valid=1;
+    core_ex_trap_valid=1;//使能外部中断线
     #30;
-    wait(core_ex_trap_ready);//响应后取消
-    core_ex_trap_valid=0;
+    wait(core_ex_trap_ready);//等待响应
+    core_ex_trap_valid=0;//取消信号
 endtask : ex_trap
 
 
@@ -159,12 +168,21 @@ generate
     end
 endgenerate
 
-always @(posedge clk) begin//显示printf时间
-    if(`CorePath.inst_csr.printf_valid && (`CorePath.inst_csr.idex_csr_wdata_i[7:0]=="\n")) begin
-        #1;
-        //$display("*Sim tool:Sim cycle = %d", sim_cycle_cnt);
-    end
-    
+//显示printf单行内容，遇到转义符n r则刷新
+always @(posedge clk) begin
+    if(`CorePath.inst_csr.printf_valid) begin
+        sim_printf_ascii = `CorePath.inst_csr.idex_csr_wdata_i[7:0];//读取printf的内容
+        if(sim_printf_ascii=="\n" || sim_printf_ascii=="\r") begin
+            sim_printf_p = sim_printf_line_length;
+            sim_printf_line = '0;
+        end
+        else begin
+            for (int printf_bitp = 0; printf_bitp < 8; printf_bitp=printf_bitp+1) begin
+                sim_printf_line[(sim_printf_p-1)*8+printf_bitp] = sim_printf_ascii[printf_bitp];
+            end
+            sim_printf_p = sim_printf_p-1;
+        end
+    end 
 end
 
 initial begin
